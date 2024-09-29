906eba6e925586b8ae80f0271ee0d10800f3d803
AndresMontoro <andres.montorovenegas@alum.uca.es>
2024-09-29 19:28:53 +0200

    Managing app with docker compose

--- semanasantaestadisticas/src/main/java/es/ca/andresmontoro/semanasantaestadisticas/estadisticas/contratos/EstadisticasContratoController.java
+++ semanasantaestadisticas/src/main/java/es/ca/andresmontoro/semanasantaestadisticas/estadisticas/contratos/EstadisticasContratoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import reactor.core.publisher.Flux;
 
@@ -17,10 +18,20 @@ public class EstadisticasContratoController {
   private final EstadisticasContratoService estadisticasContratoService;
 
   @GetMapping("/contratosCiudad/{lastNYears}")
+  @CircuitBreaker(name = "estadisticasContratos", fallbackMethod = "fallBackMethod")
   public Flux<AnnioContrato> getContratosCiudadOnPeriod(
     @PathVariable int lastNYears,
     @RequestParam(required = true) String nombreCiudad
   ) {
     return estadisticasContratoService.getContratosCiudadOnPeriod(nombreCiudad, lastNYears);
+  }
+
+  // TODO: this must return a more sofisticated error and create a timeout
+  public String fallBackMethod(
+    int lastNYears,
+    String nombreCiudad,
+    RuntimeException runtimeException
+  ) {
+    return "Something went wrong";
   }
 }
